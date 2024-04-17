de117aac5996485a35a3eff680c0157fbe521a82
Zenon Marek Raubuć <m.raubuc@wp.pl>
2024-04-17 12:57:54 +0200

    Update models

--- lib/models/user.model.ts
+++ lib/models/user.model.ts
@@ -5,7 +5,6 @@ const UserSchema = new Schema({
     name: { type: String, required: true },
     password: { type: String, required: true },
     imageUrl: { type: String },
-    results: { type: Number },
 });
 
 const User = models.User || model('User', UserSchema);
